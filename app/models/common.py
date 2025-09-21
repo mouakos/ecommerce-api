c03d66a3b295f24fed6967384e2770cdf7e43e6f
Stephane Mouako <stephane.mouako@sioux.eu>
2025-09-21 16:12:51 +0200

    feat: enhance TimestampMixin to use updated_at_column for updated_at field

--- app/models/common.py
+++ app/models/common.py
@@ -6,7 +6,7 @@ These mixins can be used to add common fields like timestamps and UUIDs to your 
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Column, DateTime, Field, SQLModel
 
 
 def utcnow() -> datetime:
@@ -14,12 +14,20 @@ def utcnow() -> datetime:
     return datetime.utcnow()
 
 
+def updated_at_column() -> Column:
+    """Updated at timestamp column."""
+    return Column(DateTime, default=utcnow, onupdate=utcnow, nullable=False)
+
+
 class TimestampMixin(SQLModel):
     """Mixin to add created_at and updated_at timestamps."""
 
     created_at: datetime = Field(default_factory=utcnow)
 
-    updated_at: datetime = Field(default_factory=utcnow)
+    updated_at: datetime = Field(
+        default_factory=utcnow,
+        sa_column=updated_at_column,
+    )
 
 
 class UUIDMixin(SQLModel):
