150e1276c53fd894f956bc97781d43f35174a3d9
Stephane Mouako <stephane.mouako@sioux.eu>
2025-11-04 01:21:20 +0100

    feat: add review table to database schema with upgrade and downgrade functions

--- alembic/env.py
+++ alembic/env.py
@@ -8,11 +8,7 @@ from sqlmodel import SQLModel
 
 from alembic import context
 from app.core.config import settings
-from app.models.category import Category 
-from app.models.product import Product
-from app.models.user import User
-from app.models.cart import Cart, CartItem
-from app.models.order import Order, OrderItem
+from app.models import *  # noqa: F403,F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
